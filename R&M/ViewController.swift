76ff647ec7a9cfa9af048e896f93e0185e904715
Vadym Katenin <vadimkatenin1@gmail.com>
2020-03-10 18:42:38 +0200

    Initial Commit

---
+++ R&M/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  R&M
+//
+//  Created by Vadim Katenin on 10.03.2020.
+//  Copyright © 2020 Vadim Katenin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
